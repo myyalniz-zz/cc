6a0a3df6d3a425b6491407ac22e5798e48092dbd
Mustafa Yalniz <myyalniz@gmail.com>
2016-09-08 13:21:28 +0300

    initial commit

---
+++ installed_packages.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get install nginx
+
+sudo apt-get install python-software-properties
+sudo apt-get install php-pear php5 php5-cli php5-common php5-curl php5-fpm php5-gd php5-json php5-mcrypt php5-mysql php5-readline
+
+sudo apt-get install mysql-server mysql-client
+
+
